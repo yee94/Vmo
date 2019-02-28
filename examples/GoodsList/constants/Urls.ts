08890d542306b958f5d09aac84932873cffe4737
xiaobe <wyy.xb@qq.com>
2019-03-01 00:29:02 +0800

    feature: 完成第一个Example

---
+++ examples/GoodsList/constants/Urls.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by yee.wang on 2019-02-28
+ **/
+
+const HOST = "https://xxxxx";
+
+export const MENU_URL = `${HOST}/menu/get`;
+export const USER_URL = `${HOST}/user/get`;
--- src/Core/Vmo.ts
+++ src/Core/Vmo.ts
@@ -5,29 +5,46 @@
 import { IDriver } from "../Interface/IDriver";
 
 export default abstract class Vmo {
+  /**
+   * 数据存储驱动器
+   */
   protected static driver: IDriver;
-  protected static requestUrl: string;
 
   /**
    * 用于转换数据到Model
    * @param data
    */
-  protected abstract builder(data: any): this;
-  protected adaptor = this.builder;
+  protected load(data: any): this {
+    Object.keys(data).forEach(key => {
+      if (this["__proto__"].hasOwnProperty(key)) {
+        this[key] = data[key];
+      }
+    });
 
-  constructor() {}
+    return this;
+  }
+
+  constructor(data?: any) {
+    if (data !== undefined) {
+      this.load(data);
+    }
+  }
 
   /**
    * 获取模型集合
    * @param options
    */
-  public static async list(...options: any): Promise<any> {}
+  public static async list(...options: any): Promise<Vmo[]> {
+    return null;
+  }
 
   /**
    * 获取模型
    * @param options
    */
-  public static async get(...options: any): Promise<any> {}
+  public static async get(...options: any): Promise<Vmo> {
+    return null;
+  }
 
   /**
    * 保存模型数据
@@ -35,4 +52,15 @@ export default abstract class Vmo {
    * @param options
    */
   public static async set(data: any, ...options: any): Promise<any> {}
+
+  public toJs(): any {
+    const keys = Object.keys(this);
+    const object: any = {};
+    keys.forEach(key => {
+      if (!/^(function|undefined)$/.test(typeof this[key])) {
+        object[key] = this[key];
+      }
+    });
+    return object;
+  }
 }
