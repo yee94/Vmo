9263b97f3e3c499ca21bfbf0a7703592e27d3016
xiaobe <wyy.xb@qq.com>
2019-03-08 22:43:37 +0800

    fixed: 修改编译为es5

--- packages/axios/bundle/index.js
+++ packages/axios/bundle/index.js
@@ -7,37 +7,255 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 var axios = _interopDefault(require('axios'));
 var base = require('@vmojs/base');
 
-// @ts-ignore
-const AxiosDriver = {
-    set: axios.post,
-    ...axios
-};
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
+  try {
+    var info = gen[key](arg);
+    var value = info.value;
+  } catch (error) {
+    reject(error);
+    return;
+  }
 
-/**
- * Created by yee.wang on 2019-02-28
- **/
-class AxiosVmo extends base.Vmo {
+  if (info.done) {
+    resolve(value);
+  } else {
+    Promise.resolve(value).then(_next, _throw);
+  }
+}
+
+function _asyncToGenerator(fn) {
+  return function () {
+    var self = this,
+        args = arguments;
+    return new Promise(function (resolve, reject) {
+      var gen = fn.apply(self, args);
+
+      function _next(value) {
+        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
+      }
+
+      function _throw(err) {
+        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
+      }
+
+      _next(undefined);
+    });
+  };
+}
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
+function _objectSpread(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i] != null ? arguments[i] : {};
+    var ownKeys = Object.keys(source);
+
+    if (typeof Object.getOwnPropertySymbols === 'function') {
+      ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) {
+        return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+      }));
+    }
+
+    ownKeys.forEach(function (key) {
+      _defineProperty(target, key, source[key]);
+    });
+  }
+
+  return target;
+}
+
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function");
+  }
+
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      writable: true,
+      configurable: true
+    }
+  });
+  if (superClass) _setPrototypeOf(subClass, superClass);
+}
+
+function _getPrototypeOf(o) {
+  _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
+    return o.__proto__ || Object.getPrototypeOf(o);
+  };
+  return _getPrototypeOf(o);
+}
+
+function _setPrototypeOf(o, p) {
+  _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+    o.__proto__ = p;
+    return o;
+  };
+
+  return _setPrototypeOf(o, p);
+}
+
+function _assertThisInitialized(self) {
+  if (self === void 0) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+  }
+
+  return self;
+}
+
+function _possibleConstructorReturn(self, call) {
+  if (call && (typeof call === "object" || typeof call === "function")) {
+    return call;
+  }
+
+  return _assertThisInitialized(self);
+}
+
+var AxiosDriver = _objectSpread({
+  set: axios.post
+}, axios);
+
+var AxiosVmo =
+/*#__PURE__*/
+function (_Vmo) {
+  _inherits(AxiosVmo, _Vmo);
+
+  function AxiosVmo() {
+    _classCallCheck(this, AxiosVmo);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(AxiosVmo).apply(this, arguments));
+  }
+
+  _createClass(AxiosVmo, null, [{
+    key: "list",
+
     /**
      * 获取模型集合
      * @param options
      */
-    static async list(...options) {
-        return null;
-    }
+    value: function () {
+      var _list = _asyncToGenerator(
+      /*#__PURE__*/
+      regeneratorRuntime.mark(function _callee() {
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                return _context.abrupt("return", null);
+
+              case 1:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      }));
+
+      function list() {
+        return _list.apply(this, arguments);
+      }
+
+      return list;
+    }()
     /**
      * 获取模型
      * @param options
      */
-    static async get(...options) {
-        return null;
-    }
+
+  }, {
+    key: "get",
+    value: function () {
+      var _get = _asyncToGenerator(
+      /*#__PURE__*/
+      regeneratorRuntime.mark(function _callee2() {
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                return _context2.abrupt("return", null);
+
+              case 1:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2);
+      }));
+
+      function get() {
+        return _get.apply(this, arguments);
+      }
+
+      return get;
+    }()
     /**
      * 保存模型数据
      * @param data
      * @param options
      */
-    static async set(data, ...options) { }
-}
+
+  }, {
+    key: "set",
+    value: function () {
+      var _set = _asyncToGenerator(
+      /*#__PURE__*/
+      regeneratorRuntime.mark(function _callee3(data) {
+        return regeneratorRuntime.wrap(function _callee3$(_context3) {
+          while (1) {
+            switch (_context3.prev = _context3.next) {
+              case 0:
+              case "end":
+                return _context3.stop();
+            }
+          }
+        }, _callee3);
+      }));
+
+      function set(_x) {
+        return _set.apply(this, arguments);
+      }
+
+      return set;
+    }()
+  }]);
+
+  return AxiosVmo;
+}(base.Vmo);
 AxiosVmo.driver = AxiosDriver;
 
 exports.AxiosVmo = AxiosVmo;
--- packages/core/bundle/index.js
+++ packages/core/bundle/index.js
@@ -2,55 +2,113 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
+function _typeof(obj) {
+  if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
+    _typeof = function (obj) {
+      return typeof obj;
+    };
+  } else {
+    _typeof = function (obj) {
+      return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+    };
+  }
+
+  return _typeof(obj);
+}
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
 /**
  * Created by yee.wang on 2019-02-28
  **/
-class Vmo {
+var Vmo =
+/*#__PURE__*/
+function () {
+  _createClass(Vmo, [{
+    key: "load",
+
     /**
      * 用于转换数据到Model
      * @param data
      */
-    load(data) {
-        Object.keys(data).forEach(key => {
-            if (this["__proto__"].hasOwnProperty(key)) {
-                this[key] = data[key];
-            }
-        });
-        return this;
-    }
-    constructor(data) {
-        if (data !== undefined) {
-            this.load(data);
+    value: function load(data) {
+      var _this = this;
+
+      Object.keys(data).forEach(function (key) {
+        if (_this["__proto__"].hasOwnProperty(key)) {
+          _this[key] = data[key];
         }
+      });
+      return this;
     }
-    toJs() {
-        const keys = Object.keys(this);
-        const object = {};
-        keys.forEach(key => {
-            if (!/^(function|undefined)$/.test(typeof this[key])) {
-                object[key] = this[key];
-            }
-        });
-        return object;
+  }]);
+
+  function Vmo(data) {
+    _classCallCheck(this, Vmo);
+
+    if (data !== undefined) {
+      this.load(data);
     }
-}
+  }
+
+  _createClass(Vmo, [{
+    key: "toJs",
+    value: function toJs() {
+      var _this2 = this;
+
+      var keys = Object.keys(this);
+      var object = {};
+      keys.forEach(function (key) {
+        if (!/^(function|undefined)$/.test(_typeof(_this2[key]))) {
+          object[key] = _this2[key];
+        }
+      });
+      return object;
+    }
+  }]);
+
+  return Vmo;
+}();
 
 /**
  * Created by yee.wang on 2019-03-01
  **/
 function mapValue(arr, instance) {
-    if (arr instanceof Array) {
-        return arr.map(data => new instance(data));
-    }
-    return null;
+  if (arr instanceof Array) {
+    return arr.map(function (data) {
+      return new instance(data);
+    });
+  }
+
+  return null;
 }
 function Field(target, name) {
-    const descr = {
-        enumerable: true,
-        configurable: true,
-        writable: true
-    };
-    return descr;
+  var descr = {
+    enumerable: true,
+    configurable: true,
+    writable: true
+  };
+  return descr;
 }
 
 exports.Vmo = Vmo;
