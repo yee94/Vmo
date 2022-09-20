de6cc8942c730ea7bd26cb5876e7184cd8d2e7d9
xiaobebe <wyy.xb@qq.com>
2022-09-20 17:53:51 +0800

    remove useless file

--- examples/GoodsList/constants/Urls.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by yee.wang on 2019-02-28
- **/
-
-const HOST = "https://xxxxx";
-
-export const MENU_URL = `${HOST}/menu/get`;
-export const USER_URL = `${HOST}/user/get`;
--- examples/GoodsList/utils/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by yee.wang on 2019-02-28
- **/
-
-export function wait(time = 0) {
-  return new Promise(resolve => setTimeout(() => resolve(), time));
-}
