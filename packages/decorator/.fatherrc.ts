90113b9531a865f62c24d79851ccaacb3fd70a47
xiaobebe <wyy.xb@qq.com>
2022-09-20 17:30:44 +0800

    feat: basic usage

---
+++ packages/decorator/.fatherrc.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "father";
+
+export default defineConfig({
+  esm: {},
+  cjs: {},
+});
---
+++ packages/decorator/test/decorator.test.ts
@@ -0,0 +1,27 @@
+import { Vmo } from "../src";
+
+@Vmo()
+export class PageParams {
+  constructor(data: any) {}
+
+  @Vmo()
+  type?: string;
+
+  // subType2 => subType
+  @Vmo("subType2")
+  subType?: string;
+
+  @Vmo(({ type, subType2 }) => `${type}_${subType2}`)
+  finalType?: string;
+}
+
+test("decorator", (t) => {
+  const instance = new PageParams({ type: "Type1", subType2: "SubType" });
+  expect(instance).toMatchInlineSnapshot(`
+      PageParams {
+        "finalType": "Type1_SubType",
+        "subType": "SubType",
+        "type": "Type1",
+      }
+    `);
+});
